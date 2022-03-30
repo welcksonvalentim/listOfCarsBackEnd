8009ce6d1fe1005e7a3d4e92d60e9ddb34bd36ef
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-03-30 17:52:29 -0300

    welckson valentim - build: insere cors dentro do app

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 
 const carsController = require('.controllers/car');
-const { getAllCars } = carsController;
 
 const app = express();
+app.use(cors());
+
 const PORT = process.env.PORT || 3000;
 
-app.get('/cars', getAllCars);
+app.get('/cars', carsController.getAllCars);
 
 app.listen(PORT, () => console.log(`Executando na porta: ${PORT}`));
